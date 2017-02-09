16a249b1652a94a451618ee61198d4b0e63888dc
Gibellino <gibellino@gmail.com>
2017-02-09 15:03:11 +0000

    Alterações

---
+++ src/model/Article.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Article {
+
+	private String nome;
+	private double preco;
+	private String descricao;
+	
+	public Article(String nome, double preco, String descricao) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+}
